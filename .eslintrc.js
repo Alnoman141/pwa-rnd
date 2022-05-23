041036bb3c57abad5055e2b1ee258ffbfdf4caf0
Alnoman141 <nmnaba14@gmail.com>
2022-05-23 18:22:50 +0600

    api response done

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     "no-mixed-spaces-and-tabs": 0, // disable rule
     "no-unused-vars": 0, // disable rule
+    "no-console": 0, // disable rule
   },
   parserOptions: {
     parser: 'babel-eslint'
